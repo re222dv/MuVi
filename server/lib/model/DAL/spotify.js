d58b2d983303b9cefa200ddcf0e1900bbf2e25b1
Rasmus Eneman <rasmus@eneman.eu>
2014-12-18 15:58:17 +0100

    fix: Ignore songs without an id

--- server/lib/model/DAL/spotify.js
+++ server/lib/model/DAL/spotify.js
@@ -24,67 +24,69 @@ let createMusicEntities = (songs) => {
   let albums = [];
   let artists = [];
 
-  songs.forEach(createSpotifyEntity((spotifyEntity, song) => {
-    let songEntities = [{
-      type: 'Song',
-      name: song.name,
-      popularity: song.popularity,
-      durationMs: song.duration_ms,
-      number: song.track_number,
-    },  spotifyEntity];
-    let songRelations = [{start: songEntities[0], end: spotifyEntity}];
+  songs
+    .filter(song => !!song.id)
+    .forEach(createSpotifyEntity((spotifyEntity, song) => {
+      let songEntities = [{
+        type: 'Song',
+        name: song.name,
+        popularity: song.popularity,
+        durationMs: song.duration_ms,
+        number: song.track_number,
+      },  spotifyEntity];
+      let songRelations = [{start: songEntities[0], end: spotifyEntity}];
 
-    if (albums.indexOf(song.album.id) === -1) { // The album haven't been instantiated
-      (createSpotifyEntity((spotifyEntity, album) => {
-        songEntities = songEntities.concat([{
-          type: 'Album',
-          name: album.name,
-          //year: song.album.release_date.split('-', 1)[0], // Only get the year
-          albumType: album.album_type,
-        }, spotifyEntity]);
-        songRelations = songRelations.concat([
-          {start: songEntities[0], end: songEntities[2]}, // Song to Album
-          {start: songEntities[2], end: songEntities[3]}, // Album to SpotifyEntity
-        ]);
-        albums.push(song.album.id);
-      }))(song.album);
-    } else {
-      let album = relations // Get the instantiated album
-        .filter(relation => relation.start.type === 'Album')
-        .filter(relation => relation.end.type === 'SpotifyEntity')
-        .filter(relation => relation.end.spotifyId === song.album.id)
-        .map(relation => relation.start)[0];
-
-      songRelations.push({start: songEntities[0], end: album}); // Song to Album
-    }
-
-    song.artists.forEach(createSpotifyEntity((spotifyEntity, artist) => {
-      if (artists.indexOf(artist.id) === -1) { // The artist haven't been instantiated
-        let artistEntities = [{
-          type: 'Artist',
-          name: artist.name,
-        }, spotifyEntity];
-
-        songEntities = songEntities.concat(artistEntities);
-        songRelations = songRelations.concat([
-          {start: songEntities[0], end: artistEntities[0]}, // Song to Artist
-          {start: artistEntities[0], end: spotifyEntity}
-        ]);
-        artists.push(artist.id);
+      if (albums.indexOf(song.album.id) === -1) { // The album haven't been instantiated
+        (createSpotifyEntity((spotifyEntity, album) => {
+          songEntities = songEntities.concat([{
+            type: 'Album',
+            name: album.name,
+            //year: song.album.release_date.split('-', 1)[0], // Only get the year
+            albumType: album.album_type,
+          }, spotifyEntity]);
+          songRelations = songRelations.concat([
+            {start: songEntities[0], end: songEntities[2]}, // Song to Album
+            {start: songEntities[2], end: songEntities[3]}, // Album to SpotifyEntity
+          ]);
+          albums.push(song.album.id);
+        }))(song.album);
       } else {
-        let oldArtist = relations // Get the instantiated artist
-          .filter(relation => relation.start.type === 'Artist')
+        let album = relations // Get the instantiated album
+          .filter(relation => relation.start.type === 'Album')
           .filter(relation => relation.end.type === 'SpotifyEntity')
-          .filter(relation => relation.end.spotifyId === artist.id)
+          .filter(relation => relation.end.spotifyId === song.album.id)
           .map(relation => relation.start)[0];
 
-        songRelations.push({start: songEntities[0], end: oldArtist}); // Song to Artist
+        songRelations.push({start: songEntities[0], end: album}); // Song to Album
       }
-    }));
 
-    entities = entities.concat(songEntities);
-    relations = relations.concat(songRelations);
-  }));
+      song.artists.forEach(createSpotifyEntity((spotifyEntity, artist) => {
+        if (artists.indexOf(artist.id) === -1) { // The artist haven't been instantiated
+          let artistEntities = [{
+            type: 'Artist',
+            name: artist.name,
+          }, spotifyEntity];
+
+          songEntities = songEntities.concat(artistEntities);
+          songRelations = songRelations.concat([
+            {start: songEntities[0], end: artistEntities[0]}, // Song to Artist
+            {start: artistEntities[0], end: spotifyEntity}
+          ]);
+          artists.push(artist.id);
+        } else {
+          let oldArtist = relations // Get the instantiated artist
+            .filter(relation => relation.start.type === 'Artist')
+            .filter(relation => relation.end.type === 'SpotifyEntity')
+            .filter(relation => relation.end.spotifyId === artist.id)
+            .map(relation => relation.start)[0];
+
+          songRelations.push({start: songEntities[0], end: oldArtist}); // Song to Artist
+        }
+      }));
+
+      entities = entities.concat(songEntities);
+      relations = relations.concat(songRelations);
+    }));
 
   return {entities, relations};
 };
