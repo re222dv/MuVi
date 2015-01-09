d9b182dc6fc041562587cb70240d6f49dcc5a20e
Rasmus Eneman <rasmus@eneman.eu>
2015-01-10 00:08:26 +0100

    chore: rewrite vulcanized elements file name in cache manifest

--- tools/setup.sh
+++ tools/setup.sh
@@ -19,3 +19,4 @@ HASH=`md5sum dist/elements/elements.vulcanized.html | awk '{print $1;}'`
 
 mv "dist/elements/elements.vulcanized.html" "dist/elements/elements.${HASH}.html"
 sed -i "s/elements\.vulcanized\.html/elements\.${HASH}\.html/" dist/index.html
+sed -i "s/elements\.vulcanized\.html/elements\.${HASH}\.html/" dist/cache.appcache
