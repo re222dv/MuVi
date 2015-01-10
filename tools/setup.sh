a9f32daa6b80bfdf585fbed0eaf1fddab7c3f50c
Rasmus Eneman <rasmus@eneman.eu>
2015-01-10 19:24:17 +0100

    chore: Cache burst appcache manifest

--- tools/setup.sh
+++ tools/setup.sh
@@ -20,3 +20,8 @@ HASH=`md5sum dist/elements/elements.vulcanized.html | awk '{print $1;}'`
 mv "dist/elements/elements.vulcanized.html" "dist/elements/elements.${HASH}.html"
 sed -i "s/elements\.vulcanized\.html/elements\.${HASH}\.html/" dist/index.html
 sed -i "s/elements\.vulcanized\.html/elements\.${HASH}\.html/" dist/cache.appcache
+
+HASH=`md5sum dist/cache.appcache | awk '{print $1;}'`
+
+mv "dist/cache.appcache" "dist/${HASH}.appcache"
+sed -i "s/cache\.appcache/${HASH}\.appcache/" dist/index.html
